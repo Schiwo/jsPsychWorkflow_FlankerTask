4f866c8069608cfcde34a1f4bec4d1fe2f66c307
Moritz Schiltenwolf <moritz.schiwo@gmail.com>
2026-02-16 12:55:13 +0100

    names

--- input/stimulus_constructor.js
+++ input/stimulus_constructor.js
@@ -1,4 +1,4 @@
-function stimulusConstructor(expInfo, trialPart, list, trialNr, blockNr, training) {
+function stimulusConstructor(expInfo, trialPart, list, trialNr, blockNr, training=false) {
   if (list) {  
     currentCongruency = list[trialNr][0];
     prevCongruency = list[trialNr][1];
